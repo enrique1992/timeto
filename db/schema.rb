38f214b3bd1fa78b4df05ae7500ee6782eff219e
alumno <alumno@example.com>
2019-12-01 21:52:59 +0100

    users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191201183425) do
+ActiveRecord::Schema.define(version: 20191201204825) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
