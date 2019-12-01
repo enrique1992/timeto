80a066ff88b6c0b39a970ad865bc5ce38e301cef
alumno <alumno@example.com>
2019-12-01 22:57:53 +0100

    notas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191201212046) do
+ActiveRecord::Schema.define(version: 20191201215230) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
