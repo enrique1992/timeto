12455b1d524578c7372fa98a8f1677c69eb8fc43
alumno <alumno@example.com>
2019-12-05 22:56:46 +0100

    Crear Sesiones

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        respond_to do |format|
+            if user && user.authenticate(params[:session][:password])            
+                session[:user_id] = user.id           
+                # redirect_to user_path(user)
+                format.html { redirect_to user_path(user), notice: 'Login Success!' }         
+            else           
+                # render 'new'
+                format.html { render 'new', notice: 'Login NOT success!' }
+            end
+        end
+        
+    end
+
+    def destroy
+        session[:user_id] = nil
+        # redirect_to root_path
+        respond_to do |format|
+            format.html { redirect_to root_path, notice: 'Logout!' }
+        end
+        
+    end
+    
+
+end
