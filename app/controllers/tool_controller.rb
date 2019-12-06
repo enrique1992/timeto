98f53973b79e1430586aab104cdfe1aba0091b50
alumno <alumno@example.com>
2019-12-06 21:29:26 +0100

    Estilos en Tool

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -5,7 +5,8 @@ class ToolController < ApplicationController
         today = today.strftime("%d/%m/%y")
         # @tasks = Task.where("user_id = ? AND day = ?", current_user, today)
         user = User.find(session[:user_id])
-        tasks_today = user.tasks.select{|task| task.day == today && task.state != "DONE"}
+        # tasks_today = user.tasks.select{|task| task.day == today && task.state != "DONE"}
+        tasks_today = user.tasks.select{|task| task.day == today}
         @tasks = tasks_today
     end
 
