7a8d6e68b479ab5fd95ceab29afc6e4cc2dd6efa
alumno <alumno@example.com>
2019-12-06 05:56:55 +0100

    Ajax Mejorado

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -4,7 +4,7 @@ class ToolController < ApplicationController
         today = today.strftime("%d/%m/%y")
         # @tasks = Task.where("user_id = ? AND day = ?", current_user, today)
         user = User.find(session[:user_id])
-        tasks_today = user.tasks.select{|task| task.day == today}
+        tasks_today = user.tasks.select{|task| task.day == today && task.state != "DONE"}
         @tasks = tasks_today
     end
 
