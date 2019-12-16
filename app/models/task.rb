d28814705f3e58ea85bfbe8a25aa29e48f8c2fcd
alumno <alumno@example.com>
2019-12-16 02:18:51 +0100

    Mas estilos

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :user
-    validates :title, presence: true, length: {minimum: 3, maximum: 50}
-    validates :description, presence: true, length: {minimum: 10, maximum: 300}
+    validates :title, presence: true, length: {minimum: 3, maximum: 30}
+    validates :description, presence: true, length: {minimum: 10, maximum: 50}
     validates :day, presence: true, length: { minimum: 8, maximum: 8}
 
 end
